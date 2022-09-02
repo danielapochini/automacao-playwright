7a9e55f925058bfafa04c065669b3c9a87e1ff65
danielapochini <daniela.pochini@fatec.sp.gov.br>
2022-09-02 18:59:36 -0300

    adding default reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,29 @@
 import type { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
+	reporter: [ ['list'], ['html', { open: 'never' }] ],
 	use: {
 		headless: true,
 		viewport: { width: 1280, height: 720 },
 		ignoreHTTPSErrors: true,
 		video: 'retain-on-failure',
-		screenshot: 'only-on-failure',
+		screenshot: 'on',
 		browserName: 'firefox',
+		
 	},
+	projects: [
+		{
+			name: 'Chromium',
+			use: { browserName: 'chromium',
+		},
+		},
+		{
+			name: 'Firefox',
+			use: { browserName: 'firefox' },
+		},
+		{
+			name: 'Webkit',
+			use: { browserName: 'webkit' },
+		},
+	],
 }
 export default config
